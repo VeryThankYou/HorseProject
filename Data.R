82122020934717f4d1b84f45af67975a04b76471
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-01-09 09:36:05 +0100

    boxplots added

--- Data.R
+++ Data.R
@@ -1,17 +1,16 @@
 D <- read.delim("horse_data23.txt")
 print(D)
-par(mfrow = c(1, 3))
-boxplot(D[D$lameLeg == "none", "A"], D[D$lameLeg != "none", "A"], col = c("red", "blue"))
-boxplot(D[D$lameLeg == "none", "S"], D[D$lameLeg != "none", "S"], col = c("red", "blue"))
-boxplot(D[D$lameLeg == "none", "W"], D[D$lameLeg != "none", "W"], col = c("red", "blue"))
-legend(1, 95, legend = c("No lameness", "Lame"))
+par(mfrow = c(1, 1))
+boxplot(D$A ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+boxplot(D$S ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+boxplot(D$W ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
 
 # If horse has a significant effect on the symmetry scores
-L_A <- lm(A ~ horse * lameLeg, data = data.horse)
+L_A <- lm(A ~ horse * lameLeg, data = D)
 anova(L_A)
 
-L_S <- lm(S ~ horse * lameLeg, data = data.horse)
+L_S <- lm(S ~ horse * lameLeg, data = D)
 anova(L_S)
 
-L_W <- lm(W ~ horse * lameLeg, data = data.horse)
+L_W <- lm(W ~ horse * lameLeg, data = D)
 anova(L_W)

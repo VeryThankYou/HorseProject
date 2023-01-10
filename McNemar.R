c1819cc42f6ff19c3699ff44b4821171e7daaecb
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-01-10 14:06:52 +0100

    McNemar section 3

--- McNemar.R
+++ McNemar.R
@@ -9,3 +9,16 @@ for(e in names)
     print(e)
     print(mcnemar.test(D))
 }
+
+print("----- Section 3 -----")
+names3 <- c("AW-Both_3.csv", "AW-PC3PC4_3.csv", "Baseline-AW_3.csv", "Baseline-Both_3.csv", "Baseline-PC3PC4_3.csv", "PC3PC4-Both_3.csv")
+
+for(e in names3)
+{
+    print(e)
+    D <- read.csv(e, header = T)
+    D <- D[, c("int1", "int2")]
+    D <- data.matrix(D)
+    
+    print(mcnemar.test(D))
+}

0d047c62f41f63b0503b53b48aae7ba8f4054e62
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-01-11 09:14:07 +0100

    horse boxplot

--- Data.R
+++ Data.R
@@ -1,9 +1,16 @@
 D <- read.delim("horse_data23.txt")
-print(D)
+
 par(mfrow = c(1, 1))
 boxplot(D$A ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
 boxplot(D$S ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
 boxplot(D$W ~ D$lameLeg, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+
+par(mfrow = c(3, 1))
+
+boxplot(D$A ~ D$horse, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+boxplot(D$S ~ D$horse, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+boxplot(D$W ~ D$horse, col = c("red", "blue", "green", "yellow", "white"), horizontal = TRUE)
+
 
 # If horse has a significant effect on the symmetry scores
 L_A <- lm(A ~ horse * lameLeg, data = D)

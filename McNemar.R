3ab36d63f3c9b205a11049e055eeab6ab154c8ec
clarachristiansen <s214659@dtu.dk>
2023-01-12 09:19:05 +0100

    new values

--- McNemar.R
+++ McNemar.R
@@ -1,5 +1,11 @@
 
+# Set working directory
+setwd("/Users/clarasofiechristiansen/Documents/Github_Desktop/Horseproject")
+
+
 names <- c("AW-Both.csv", "AW-PC3PC4.csv", "Baseline-AW.csv", "Baseline-Both.csv", "Baseline-PC3PC4.csv", "PC3PC4-Both.csv")
+
+d_section2 <- {}
 
 for(e in names)
 {
@@ -8,10 +14,12 @@ for(e in names)
     D <- data.matrix(D)
     print(e)
     print(mcnemar.test(D))
+    d_section2[e] = mcnemar.test(D)$p.value
 }
 
 print("----- Section 3 -----")
-names3 <- c("AW-Both_3.csv", "AW-PC3PC4_3.csv", "Baseline-AW_3.csv", "Baseline-Both_3.csv", "Baseline-PC3PC4_3.csv", "PC3PC4-Both_3.csv")
+names3 <- c("AW_2-AW_3.csv", "PC3PC4_2-PC3PC4_3.csv", "Both_2-Both_3.csv", "Baseline_2-Baseline_3.csv")
+d_section3 <- {}
 
 for(e in names3)
 {
@@ -21,4 +29,16 @@ for(e in names3)
     D <- data.matrix(D)
     
     print(mcnemar.test(D))
+    d_section3[e] = mcnemar.test(D)$p.value
 }
+
+
+for_export2 <- signif(t(data.frame(d_section2)),3)
+
+
+write.csv(for_export2, "pvalues_mcnemar_2.csv")
+
+for_export3 <- signif(t(data.frame(d_section3)),3)
+
+write.csv(for_export3, "pvalues_mcnemar_3.csv")
+
--- McNemar_matrix_section3.py
+++ McNemar_matrix_section3.py
@@ -9,7 +9,8 @@ Created on Tue Jan 10 12:52:41 2023
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("predictions_section3_horseout.csv")
+df2 = pd.read_csv("predictions_section2_horseout.csv")
+df3 = pd.read_csv("predictions_section3_horseout.csv")
 
 
 d = {}
@@ -17,26 +18,25 @@ d = {}
 names = ["True", "Baseline", "AW", "PC3PC4", "Both"]
 
 for i in range(1,len(names)):
-    for j in range(i+1, len(names)):
-        #n11
-        n11 = sum((df[names[i]] == df["True"]) * (df[names[j]] == df["True"]))
+    #n11
+    n11 = sum((df2[names[i]] == df2["True"]) * (df3[names[i]] == df3["True"]))
 
-        #n12 # Baseline True
-        n12 = sum((df[names[i]] == df["True"]) * (df[names[j]] != df["True"]))
+    #n12 # Baseline True
+    n12 = sum((df2[names[i]] == df2["True"]) * (df3[names[i]] != df3["True"]))
 
-        #n21 # A/W True
-        n21 = sum((df[names[i]] != df["True"]) * (df[names[j]] == df["True"]))
+    #n21 # A/W True
+    n21 = sum((df2[names[i]] != df2["True"]) * (df3[names[i]] == df3["True"]))
 
-        #n22
-        n22 = sum((df[names[i]] != df["True"]) * (df[names[j]] != df["True"]))
-        
-        print([[n11,n12],[n21,n22]])
+    #n22
+    n22 = sum((df2[names[i]] != df2["True"]) * (df3[names[i]] != df3["True"]))
+    
+    print([[n11,n12],[n21,n22]])
 
-        matrix = pd.DataFrame([[n11,n12],[n21,n22]])
-        matrix = matrix.rename(columns={0: "int1", 1: "int2"}, index={0: "int1", 1: "int2"})
-        d[names[i] + "-" + names[j]] = matrix
+    matrix = pd.DataFrame([[n11,n12],[n21,n22]])
+    matrix = matrix.rename(columns={0: "int1", 1: "int2"}, index={0: "int1", 1: "int2"})
+    d[names[i] + "_2-" + names[i]+ "_3"]  = matrix
         
 print(d)
 
 for i in d:
-    d[i].to_csv(i + "_3"+ ".csv")
+    d[i].to_csv(i + ".csv")

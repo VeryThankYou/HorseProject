ed9ceb09d1b99c4b4aeaca2dc98b5d51ae549534
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2023-01-13 13:43:42 +0100

    Collapse added

--- McNemar.R
+++ McNemar.R
@@ -1,6 +1,4 @@
 
-# Set working directory
-setwd("/Users/clarasofiechristiansen/Documents/Github_Desktop/Horseproject")
 
 
 names <- c("AW-Both.csv", "AW-PC3PC4.csv", "Baseline-AW.csv", "Baseline-Both.csv", "Baseline-PC3PC4.csv", "PC3PC4-Both.csv")
@@ -54,3 +52,31 @@ acc_dataframe3 <- read.csv("accuracy_section3_horseout.csv", header = T)
 acc_dataframe3 <- signif(acc_dataframe3,3)*100
 acc_dataframe3[] <- lapply(acc_dataframe3, paste0, '%')
 write.csv(acc_dataframe3, "accuracy_procent_section3_horseout.csv")
+
+
+print("----- Section 2-3 collapse -----")
+names3collapse <- c("AW_2-AW_3_collapse.csv", "PC3PC4_2-PC3PC4_3_collapse.csv", "Both_2-Both_3_collapse.csv", "Baseline_2-Baseline_3_collapse.csv")
+d_section3collapse <- {}
+
+for(e in names3collapse)
+{
+    print(e)
+    D <- read.csv(e, header = T)
+    D <- D[, c("int1", "int2")]
+    D <- data.matrix(D)
+    
+    print(mcnemar.test(D))
+    d_section3collapse[e] = mcnemar.test(D)$p.value
+}
+
+
+for_export3collapse <- signif(t(data.frame(d_section3collapse)),3)
+
+write.csv(for_export3collapse, "pvalues_mcnemar_3_collapse.csv")
+
+
+# Accuracy (round to 3 significant figures)
+acc_dataframe3collapse <- read.csv("accuracy_section2_horseout_collapse.csv", header = T)
+acc_dataframe3collapse <- signif(acc_dataframe3collapse,3)*100
+acc_dataframe3collapse[] <- lapply(acc_dataframe3collapse, paste0, '%')
+write.csv(acc_dataframe3collapse, "accuracy_procent_section2_horseout_collapse.csv")
